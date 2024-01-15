819e62889b38a92009a93d3d24f7c632d4ad3b7c
DaeHan Jang <abouttt57@gmail.com>
2024-01-15 17:33:18 +0900

    [Silver I] Title: 쿼드트리, Time: 0 ms, Memory: 2164 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.cc
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.cc
@@ -11,9 +11,6 @@ using namespace std;
 using ll = long long;
 #define FAST_IO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
-int dy[] = { -1,0,1,0 };
-int dx[] = { 0,1,0,-1 };
-
 int n;
 char a[65][65];
 
